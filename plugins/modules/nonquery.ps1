3f8bec15c1ce951899514f6a48e62a6c9fad3ada
John McCall <john@lowlydba.com>
2022-02-09 16:04:42 -0500

    add license

--- plugins/modules/nonquery.ps1
+++ plugins/modules/nonquery.ps1
@@ -1,4 +1,8 @@
 #!powershell
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #AnsibleRequires -CSharpUtil Ansible.Basic
 #AnsibleRequires -PowerShell ansible_collections.lowlydba.sqlserver.plugins.module_utils._SqlServerUtils
