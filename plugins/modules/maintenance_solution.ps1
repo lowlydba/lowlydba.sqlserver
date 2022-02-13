61a5ae8e96d45fcf49bf87f7eb98448ee40e78ef
John McCall <john@lowlydba.com>
2022-02-13 10:23:51 -0500

    add maintenance_solution module

---
+++ plugins/modules/maintenance_solution.ps1
@@ -0,0 +1,105 @@
+#!powershell
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell ansible_collections.lowlydba.sqlserver.plugins.module_utils._SqlServerUtils
+
+Import-ModuleDependency
+$ErrorActionPreference = "Stop"
+
+$spec = @{
+    supports_check_mode = $true
+    options = @{
+        sql_instance = @{type = 'str'; required = $true }
+        sql_username = @{type = 'str'; required = $false }
+        sql_password = @{type = 'str'; required = $false; no_log = $true }
+        backup_location = @{type = 'str'; required = $false }
+        cleanup_time = @{type = 'int'; required = $false; }
+        output_file_dir = @{type = 'str'; required = $false }
+        replace_existing = @{type = 'bool'; required = $false; }
+        log_to_table = @{type = 'bool'; required = $false; default = $false }
+        solution = @{type = 'str'; required = $false; choices = @('All', 'Backup', 'IntegrityCheck', 'IndexOptimize'); default = 'All' }
+        install_jobs = @{type = 'bool'; required = $false; default = $false }
+        local_file = @{type = 'str'; required = $false }
+        database = @{type = 'str'; required = $true }
+        force = @{type = 'bool'; required = $false; default = $false }
+        install_parallel = @{type = 'bool'; required = $false; default = $false }
+    }
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+$sqlInstance = $module.Params.sql_instance
+$sqlUsername = $module.Params.sql_username
+if ($null -ne $sqlUsername) {
+    [securestring]$secPassword = ConvertTo-SecureString $module.Params.sql_password -AsPlainText -Force
+    [pscredential]$sqlCredential = New-Object System.Management.Automation.PSCredential ($sqlUsername, $secPassword)
+}
+$database = $module.Params.database
+$backupLocation = $module.Params.backup_location
+$outputFileDirectory = $module.Params.output_file_dir
+$cleanupTime = $module.Params.cleanup_time
+$replaceExisting = $module.Params.replace_existing
+$solution = $module.Params.solution
+$installJobs = $module.Params.install_jobs
+$installParallel = $module.Params.install_parallel
+$logToTable = $module.Params.log_to_table
+$localFile = $module.Params.local_file
+$force = $module.Params.force
+$module.Result.changed = $false
+
+try {
+    $maintenanceSolutionSplat = @{
+        SqlInstance = $sqlInstance
+        SqlCredential = $sqlCredential
+        Database = $database
+        LogToTable = $logToTable
+        Solution = $solution
+        InstallJobs = $installJobs
+        InstallParallel = $installParallel
+        Force = $force
+        Confirm = $false
+        EnableException = $true
+    }
+    if ($null -ne $localFile) {
+        $maintenanceSolutionSplat.LocalFile = $localFile
+    }
+    if ($null -ne $backupLocation) {
+        $maintenanceSolutionSplat.Branch = $backupLocation
+    }
+    if ($null -ne $outputFileDirectory) {
+        $maintenanceSolutionSplat.OutputFileDirectory = $outputFileDirectory
+    }
+    if ($installJobs -eq $true -and $null -ne $cleanupTime) {
+        $maintenanceSolutionSplat.CleanupTime = $cleanupTime
+    }
+    # Only pass if true, otherwise removes warning that is used to track changed=$false
+    if ($replaceExisting -eq $true) {
+        $maintenanceSolutionSplat.ReplaceExisting = $replaceExisting
+    }
+
+    try {
+        $output = Install-DbaMaintenanceSolution @maintenanceSolutionSplat
+        $module.Result.changed = $true
+    }
+    catch {
+        $errMessage = $_.Exception.Message
+        if ($errMessage -like "*Maintenance Solution already exists*") {
+            $connection = Connect-DbaInstance -SqlInstance $sqlInstance -SqlCredential $sqlCredential | Select-Object -Property ComputerName, InstanceName, SqlInstance
+            $connection | Add-Member -MemberType NoteProperty -Name "Results" -Value "Success"
+            $output = $connection
+        }
+        else {
+            Write-Error -Message $errMessage
+        }
+    }
+
+    $outputHash = ConvertTo-HashTable -Object $output
+    $module.Result.data = $outputHash
+    $module.ExitJson()
+}
+catch {
+    $module.FailJson("Installing Maintenance Solution failed.", $_.Exception.Message)
+}
---
+++ plugins/modules/maintenance_solution.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r'''
+---
+module: maintenance_solution
+short_description: Install/update Maintenance Solution
+description:
+  - A wrapper for Install-DbaMaintenanceSolution to fetch the latest version of the Ola Hallengren's Maintenance Solution,
+    or install from a local cached version.
+options:
+  backup_location:
+    description:
+      - Location of the backup root directory. If this is not supplied, the default backup directory will be used.
+    type: str
+    required: false
+  cleanup_time:
+    description:
+      - Time in hours, after which backup files are deleted.
+    type: int
+    required: false
+    default: 0
+  output_file_dir:
+    description:
+      - Specify the output file directory where the Maintenance Solution will write to.
+    type: str
+    required: false
+  replace_existing:
+    description:
+        - If this switch is enabled, objects already present in the target database will be dropped and recreated.
+    type: bool
+    required: false
+    default: false
+  log_to_table:
+    description:
+      - If this switch is enabled, the Maintenance Solution will be configured to log commands to a table.
+    type: bool
+    required: false
+    default: false
+  solution:
+    description:
+      - Specifies which portion of the Maintenance solution to install. Valid values are All (full solution), Backup, IntegrityCheck and IndexOptimize.
+    type: str
+    required: false
+    default: 'All'
+    choices: ['All', 'Backup', 'IntegrityCheck', 'IndexOptimize']
+  install_jobs:
+    description:
+      - If this switch is enabled, the corresponding SQL Agent Jobs will be created.
+    type: bool
+    required: false
+    default: false
+  install_parallel:
+    description:
+      - If this switch is enabled, the Queue and QueueDatabase tables are created, for use when @DatabasesInParallel = 'Y' are set in the jobs.
+    type: bool
+    required: false
+    default: false
+  local_file:
+    description:
+      - Specifies the path to a local file to install Ola's solution from. This should be the zip file as distributed by the maintainers.
+        If this parameter is not specified, the latest version will be downloaded and installed
+        from https://github.com/olahallengren/sql-server-maintenance-solution.
+    type: str
+    required: false
+  database:
+    description:
+      - Name of the target database.
+    type: str
+    required: true
+  force:
+    description:
+      - If this switch is enabled, the Maintenance Solution will be downloaded from the internet even if previously cached.
+    type: bool
+    default: false
+author: "John McCall (@lowlydba)"
+notes:
+    - Check mode is not supported.
+extends_documentation_fragment:
+  - lowlydba.sqlserver.sql_credentials
+'''
+
+EXAMPLES = r'''
+- name: Install/Update Maintenance Solution
+  lowlydba.sqlserver.multitool:
+    sql_instance: sql-01.myco.io
+    database_name: main
+    replace_existing: true
+'''
+
+RETURN = r'''
+data:
+  description: Raw output from the Install-MaintenanceSolution function.
+  returned: success
+  type: dict
+'''
