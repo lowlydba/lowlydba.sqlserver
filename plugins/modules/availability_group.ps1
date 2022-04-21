7743e55fde41fb56cc13ec13087b930ba053c082
John McCall <john@lowlydba.com>
2022-04-21 00:18:17 -0400

    yisss

--- plugins/modules/availability_group.ps1
+++ plugins/modules/availability_group.ps1
@@ -101,13 +101,13 @@ $checkMode = $module.CheckMode
 $module.Result.changed = $false
 
 try {
-    $server = Connect-DbaInstance -SqlInstance $sqlInstance -SqlCredential $sqlCredential
+    #$server = Connect-DbaInstance -SqlInstance $sqlInstance -SqlCredential $sqlCredential
     $existingAG = Get-DbaAvailabilityGroup -SqlInstance $sqlInstance -SqlCredential $sqlCredential -AvailabilityGroup $agName -EnableException
 
     if ($state -eq "present") {
         $agSplat = @{
-            #Primary = $sqlInstance
-            # PrimarySqlCredential = $sqlCredential
+            Primary = $sqlInstance
+            PrimarySqlCredential = $sqlCredential
             Name = $agName
             SeedingMode = $seedingMode
             FailoverMode = $failoverMode
@@ -159,7 +159,7 @@ try {
                     $null = Backup-DbaDatabase $backupSplat
                 }
             }
-            $output = $server | New-DbaAvailabilityGroup -Name "IntAG1" -Confirm:$false #@agSplat
+            $output = New-DbaAvailabilityGroup @agSplat
             $module.Result.changed = $true
         }
         # Configure existing AG
