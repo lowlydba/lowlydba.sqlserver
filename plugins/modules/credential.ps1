d80dc2fbbbd516f4d4cde55fb436c85bb4ba6215
Joe K <jkrilov@gmail.com>
2022-12-08 17:23:48 -0500

    Feat: simplify output for no changes
    Co-authored-by: John McCall <john@lowlydba.com>

--- plugins/modules/credential.ps1
+++ plugins/modules/credential.ps1
@@ -97,14 +97,8 @@ try {
             }
         }
         # Return existing credential if nothing is changed
-        else {
-            try {
-                $output = Get-DbaCredential @getCredendtialSplat
-                $module.result.changed = $false
-            }
-            catch {
-                $module.FailJson("Configuring credential failed: $($_.Exception.Message)", $_)
-            }
+        elseif ($null -ne $existingCredential) {
+            $output = $existingCredential
         }
     }
     try {
