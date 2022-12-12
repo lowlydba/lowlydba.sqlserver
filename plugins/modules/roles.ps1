b6303685babf3f6d849f683aa5c4f9382c22aa64
Joe K <jkrilov@gmail.com>
2022-12-12 18:51:28 +0000

    add: role module

---
+++ plugins/modules/roles.ps1
@@ -0,0 +1,114 @@
+#!powershell
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell ansible_collections.lowlydba.sqlserver.plugins.module_utils._SqlServerUtils
+#Requires -Modules @{ ModuleName="dbatools"; ModuleVersion="1.1.112" }
+
+$ErrorActionPreference = "Stop"
+
+$spec = @{
+    supports_check_mode = $true
+    options = @{
+        database = @{type = 'str'; required = $true }
+        username = @{type = 'str'; required = $true }
+        roles = @{type = 'list'; elements='str'; required = $true }
+        state = @{type = 'str'; required = $false; default = 'present'; choices = @('present', 'absent') }
+    }
+}
+
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec, @(Get-LowlyDbaSqlServerAuthSpec))
+$sqlInstance, $sqlCredential = Get-SqlCredential -Module $module
+$username = $module.Params.username
+$database = $module.Params.database
+$roles = $module.Params.roles
+$state = $module.Params.state
+$checkMode = $module.CheckMode
+
+$module.Result.changed = $false
+
+$getRoleSplat = @{
+    SqlInstance = $sqlInstance
+    SqlCredential = $sqlCredential
+    Database = $database
+    EnableException = $true
+}
+$existingRoleObjects = Get-DbaDbRoleMember @getRoleSplat | where {$_.UserName -eq $username}
+
+if ($state -eq "absent") {
+    # loop through all roles to remove and see if they are assigned to the user
+    $removeRoles = @()
+    foreach ($roleObject in $existingRoleObjects) {
+        if ($roles -Contains $roleObject.role) {
+            $removeRoles += $roleObject.role
+        }
+    }
+    if ($null -ne $removeRoles) {
+        try {
+            $removeRolesSplat = @{
+                SqlInstance = $sqlInstance
+                SqlCredential = $sqlCredential
+                User = $username
+                Database = $database
+                Role = $removeRoles -join ","
+                EnableException = $true
+                WhatIf = $checkMode
+                Confirm = $false
+            }
+            $output = Remove-DbaDbRoleMember @removeUserSplat
+            $module.Result.changed = $true
+        }
+        catch {
+            $module.FailJson("Removing role failed: $($_.Exception.Message)", $_)
+        }
+    }
+    else {
+        $output = $existingRoleObjects
+    }
+}
+elseif ($state -eq "present") {
+    $existingRoles = @()
+    # build an array of roles for the selected user
+    foreach ($roleObject in $existingRoleObjects) {
+        $existingRoles += $roleObject.role
+    }
+    # compare the list of roles to add vs the existing roles for the user and get the difference
+    $addRoles = $roles | where {$existingRoles -NotContains $_}
+    if ($null -ne $addRoles) {
+        try {
+            # No Set-DbaDbUser command exists, use SMO
+            $addRolesSplat = @{
+                SqlInstance = $sqlInstance
+                SqlCredential = $sqlCredential
+                User = $username
+                Database = $database
+                Role = $addRoles -join ","
+                EnableException = $true
+                WhatIf = $checkMode
+                Confirm = $false
+            }
+            $output = Add-DbaDbRoleMember @addRolesSplat
+            $module.Result.changed = $true
+        }
+        catch {
+            $module.FailJson("Adding role failed: $($_.Exception.Message)", $_)
+        }
+    }
+    else {
+        $output = $existingRoleObjects
+    }
+}
+try {
+    if ($null -ne $output) {
+        $resultData = ConvertTo-SerializableObject -InputObject $output
+        $module.Result.data = $resultData
+    }
+    $module.ExitJson()
+}
+catch {
+    $module.FailJson("Failure: $($_.Exception.Message)", $_)
+}
---
+++ plugins/modules/roles.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r'''
+---
+module: user
+short_description: Add or remove one or more roles for a given user in a specific database.
+description:
+  - Add or remove one or more roles for a given user in a specific database. Returns a list of current roles if there are no changes.
+version_added: 2.4.0
+options:
+  username:
+    description:
+      - Name of the user.
+    type: str
+    required: true
+  database:
+    description:
+      - Database for the user.
+    type: str
+    required: true
+  roles:
+    description:
+      - Specifies a comma separated list of roles to add or remove
+    type: list
+    elements: str
+    required: true
+
+author:
+  - "Joe Krilov (@joey40)"
+  - "John McCall (@lowlydba)"
+requirements:
+  - L(dbatools,https://www.powershellgallery.com/packages/dbatools/) PowerShell module
+extends_documentation_fragment:
+  - lowlydba.sqlserver.sql_credentials
+  - lowlydba.sqlserver.attributes.check_mode
+  - lowlydba.sqlserver.attributes.platform_all
+  - lowlydba.sqlserver.state
+'''
+
+EXAMPLES = r'''
+- name: Add a single role for a user
+  lowlydba.sqlserver.role:
+    sql_instance: sql-01.myco.io
+    username: TheIntern
+    database: InternProject1
+    role: db_datareader
+
+- name: Add multiple roles for a user
+  lowlydba.sqlserver.role:
+    sql_instance: sql-01.myco.io
+    username: TheIntern
+    database: InternProject1
+    role: db_datareader, db_datawriter
+
+- name: Remove roles for a user
+  lowlydba.sqlserver.role:
+    sql_instance: sql-01.myco.io
+    username: TheIntern
+    database: InternProject1
+    role: db_datareader, db_datawriter
+    state: absent
+'''
+
+RETURN = r'''
+data:
+  description: Output from the C(Add-DbaDbRoleMember), C(Get-DbaDbRoleMember), or C(Remove-DbaDbRoleMember) function.
+  returned: success, but not in check_mode.
+  type: dict
+'''
