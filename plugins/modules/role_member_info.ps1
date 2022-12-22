c74bff7722bb111dcb310b14e63a6c5846e711ce
Joe K <jkrilov@gmail.com>
2022-12-22 16:32:55 +0000

    add: role_member_info module

---
+++ plugins/modules/role_member_info.ps1
@@ -0,0 +1,61 @@
+#!powershell
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell ansible_collections.lowlydba.sqlserver.plugins.module_utils._SqlServerUtils
+#Requires -Modules @{ ModuleName="dbatools"; ModuleVersion="1.1.112" }
+
+$ErrorActionPreference = "Stop"
+
+$spec = @{
+    supports_check_mode = $true
+    options = @{
+        database = @{type = 'str'; required = $false }
+        username = @{type = 'str'; required = $false }
+        roles = @{type = 'list'; elements = 'str'; required = $false }
+    }
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec, @(Get-LowlyDbaSqlServerAuthSpec))
+$sqlInstance, $sqlCredential = Get-SqlCredential -Module $module
+$username = $module.Params.username
+$database = $module.Params.database
+$roles = $module.Params.roles
+$state = $module.Params.state
+$checkMode = $module.CheckMode
+
+$module.Result.changed = $false
+
+try {
+    $getRoleSplat = @{
+        SqlInstance = $sqlInstance
+        SqlCredential = $sqlCredential
+        EnableException = $true
+    }
+    if ($null -ne $roles) {
+        $getRoleSplat.Add("Role", $roles)
+    }
+    if ($null -ne $database) {
+        $getRoleSplat.Add("Database", $database)
+    }
+    if ($null -ne $username) {
+        $output = Get-DbaDbRoleMember @getRoleSplat | Where-Object { $_.UserName -eq $username }
+    }
+    else {
+        $output = Get-DbaDbRoleMember @getRoleSplat
+    }
+
+    if ($null -ne $output) {
+        $resultData = ConvertTo-SerializableObject -InputObject $output
+        $module.Result.data = $resultData
+    }
+    
+    $module.ExitJson()
+
+}
+catch {
+    $module.FailJson("Failure: $($_.Exception.Message)", $_)
+}
---
+++ plugins/modules/role_member_info.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2022, John McCall (@lowlydba)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r'''
+---
+module: role_member_info
+short_description: Returns basic information about a role or roles
+description:
+  - Returns basic information about a role or roles.
+version_added: 1.4.0
+author:
+  - "Joe Krilov (@joey40)"
+  - "John McCall (@lowlydba)"
+requirements:
+  - L(dbatools,https://www.powershellgallery.com/packages/dbatools/) PowerShell module
+extends_documentation_fragment:
+  - lowlydba.sqlserver.sql_credentials
+  - lowlydba.sqlserver.attributes.check_mode_read_only
+  - lowlydba.sqlserver.attributes.platform_all
+'''
+
+EXAMPLES = r'''
+- name: Return member of the db_datareader and db_datawriter role on the 'InternProject1' DB
+  lowlydba.sqlserver.role_member_info:
+    sql_instance: sql-01.myco.io
+    database: InternProject1
+    role: db_datareader, db_datawriter
+
+
+- name: Return all roles for user 'TheIntern' on the 'InternProject1' DB
+  lowlydba.sqlserver.role_member_info:
+    sql_instance: sql-01.myco.io
+    username: TheIntern
+    database: InternProject1
+'''
+
+RETURN = r'''
+data:
+  description: Output from the C(Get-DbaDbRoleMember) function.
+  returned: always
+  type: dict
+'''
