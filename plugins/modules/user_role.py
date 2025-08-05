40e634ebe7d7a5ce2cd181c5816ed844284a137d
Cameron Hart <camster342@gmail.com>
2025-08-05 05:19:34 +0000

    updated documentation for new option

--- plugins/modules/user_role.py
+++ plugins/modules/user_role.py
@@ -33,6 +33,7 @@ options:
     type: boolean
     required: false
     default: false
+    version_added: 2.6.2
 author: "John McCall (@lowlydba)"
 requirements:
   - L(dbatools,https://www.powershellgallery.com/packages/dbatools/) PowerShell module
